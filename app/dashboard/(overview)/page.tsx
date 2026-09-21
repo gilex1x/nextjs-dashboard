130551e28f28515e9ccc40668bffafca1a45a7f5
unknown <gilbertosa099@outlook.com>
2026-09-21 17:00:52 -0500

    [ADD]: Separate loading content from page to each component.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,15 @@
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
-    const data = await Promise.all([fetchRevenue(), fetchLatestInvoices(), fetchCardData()]);
-    const [revenueData, latestInvData, cardData] = data;
-    const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = cardData;
+
+    const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -24,8 +26,12 @@ export default async function Page() {
                 />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenueData} />
-                <LatestInvoices latestInvoices={latestInvData} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
