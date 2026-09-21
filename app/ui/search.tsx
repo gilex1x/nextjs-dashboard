f349fae62991159e4dd637115a90895957cb0a57
unknown <gilbertosa099@outlook.com>
2026-09-21 18:28:22 -0500

    [ADD]: Search input with query search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,20 @@
 'use client';
-
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+  const handleChange = (text: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (text) {
+      params.set('query', text);
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +23,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => { handleChange(event.target.value) }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
